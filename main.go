41db95cd15418a5db5cab8d9af480e9e0f2d8b7c
andrea <andrea.ceresoli03@gmail.com>
2022-02-22 20:00:44 +0100

    automated commit 22/02/2022 20:00

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+// commento pazzo
+
 var vocali = map[rune]bool{'A': true, 'E': true, 'I': true, 'O': true, 'U': true}
 
 var monthToLetter = map[int]string{1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "H", 7: "L", 8: "M", 9: "P", 10: "R", 11: "S", 12: "T"}
